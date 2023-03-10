6165bbc5d18a65f9137397e276acf51405762f38
Roi Martinez <roimarful@gmail.com>
2023-03-10 13:36:36 +0000

    Create global.R

---
+++ analysis_toolbox/global.R
@@ -0,0 +1,6 @@
+## Load required libraries
+
+library(lubridate)  ## this package helps with manage date fields 
+library(dplyr)      ## R Package to use pipelines (%>%) analysis language 
+library(sf)         ## R Package for spatial analysis in R ( Simple Features )
+library(ggplot2)    ## R Package for plotitng and graphs
