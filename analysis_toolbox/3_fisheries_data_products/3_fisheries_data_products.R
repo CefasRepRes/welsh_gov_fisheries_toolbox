7d810ad6beb1d08d4e2c6cdb9906fc842d067f3e
Mike Day <michael.day@cefas.gov.uk>
2023-06-12 13:37:45 +0100

    Finalise plotting scripts for data products

--- analysis_toolbox/3_fisheries_data_products/3_fisheries_data_products.R
+++ analysis_toolbox/3_fisheries_data_products/3_fisheries_data_products.R
@@ -1,71 +1,60 @@
-year = 2022
-
 # 3.1 Load TABLE 1 (VMS) and TABLE 2 (LOGBOOK) --------------------------------------------
-
-#######
-# DELETE BEFORE PUSH #
-#######
 
 library(tidyverse)
 library(lubridate)
-library(xlsx)
+library(vmstools)
+library(sf)
+library(rnaturalearth)
 
- 
+# function for plotting data products - to be used later, it is just common practice to put functions at the top of scripts
+plot_function = function ( data_plot , col_plot ,col_facet1 = NULL  , col_facet2 = NULL  ) {
+  
+  gg1 =  ggplot() + geom_sf( data =   world     ) +
+    geom_sf (data = data_plot , aes(color = sum_intv,  fill = sum_intv) ) +
+    theme_bw() +
+    coord_sf(xlim = lons, ylim = lats, expand = FALSE) #+
+    #labs(fill = "Effort") # +
+  # facet_wrap( ~ get( col_facet1) + get( col_facet2)  )
+  if ( is.null( col_facet2 )  )  {
+    gg1 =  gg1 +
+      facet_wrap( ~ get( col_facet1)  )
+  } else  {
+    gg1 =  gg1 +
+      facet_wrap( ~ get( col_facet1) +get( col_facet2)   )
+  }
+  return(gg1)
+}
+
+
+plot_function = function ( data_plot , col_plot ,col_facet1 = NULL  , col_facet2 = NULL  ) {
+  
+  gg1 =  ggplot() + geom_sf( data =   world     ) +
+    geom_sf (data = data_plot , aes(color = get( col_plot),  fill = get(col_plot)) ) +
+    theme_bw() +
+    coord_sf(xlim = lons, ylim = lats, expand = FALSE) +
+    labs(fill = "Effort") # +
+  # facet_wrap( ~ get( col_facet1) + get( col_facet2)  )
+  if ( is.null( col_facet2 )  )  {
+    gg1 =  gg1 +
+      facet_wrap( ~ get( col_facet1)  )
+  } else  {
+    gg1 =  gg1 +
+      facet_wrap( ~ get( col_facet1) +get( col_facet2)   )
+  }
+  return(gg1)
+}
+
+
+inPath = "C:\\Users\\md09\\OneDrive - CEFAS\\projects\\C8529A-welsh-gov\\data\\data-2-output"
+
 #######
 
-load(file = paste0(outPath, "/eflalo_output_", year , ".RData")  )
-load(file = paste0(outPath, "/tacsatEflalo_output_", year , ".RData")  )
+year = 2022
+
+load(file = paste0(inPath, "/eflalo_output_", year , ".RData")  )
+load(file = paste0(inPath, "/tacsatEflalo_output_", year , ".RData")  )
 
 ## 3.2 Welsh Gov Fisheries Data Product 
-
-
-head(tacsatEflalo)
-
-# table1Save <-
-#   table1 %>%
-# 
-#   group_by(RT,VE_COU,Year,Month,Csquare,LE_GEAR, met5, LE_MET,LENGTHCAT) %>%
-#   summarise(
-#     mean_si_sp       = mean(SI_SP),
-#     sum_intv         = sum(INTV, na.rm=TRUE),
-#     mean_intv        = mean(INTV, na.rm=TRUE),
-#     mean_ve_len      = mean(VE_LEN, na.rm = TRUE),
-#     mean_ve_kf       = mean(VE_KW, na.rm = TRUE),
-#     sum_kwHour       = sum(kwHour, na.rm=TRUE),
-#     sum_le_kg_tot    = sum(LE_KG_TOT, na.rm = TRUE),
-#     sum_le_euro_tot  = sum(LE_EURO_TOT, na.rm = TRUE),
-#     n_vessels        = n_distinct(VE_ID, na.rm = TRUE),
-#     vessel_ids       = ifelse (
-#       n_distinct(VE_ID) < 3,
-#       paste(unique(VE_ID), collapse = ";"),
-#       'not_required'
-#     )
-#   ) %>%  relocate( n_vessels,vessel_ids, .before = Csquare)%>%
-#   mutate (AverageGearWidth = NA%>%as.numeric()  )%>% ## If this information is available modify this line of the script. By default is assumed not existing gear width information
-#   as.data.frame()
-
-# TABLE 1.
-
-#Fishing activity indicators: 
-
-  # Total Fishing effort hours
-  # Total Fishing effort hours * kw
-  # Total Fishing catch weight
-  # Fishing catch value 
-  # Average speed
-  # Average length 
-  # Average Kw 
-
-
-# aggregated by: 
-
-  # Temporal resolution: year, month 
-  # Spatial resolution:  c_square 0.05
-  # Fleet resolution: m4, m5 and vessel length classes
-
-
-
-
 
 # Create vessel length class: Add the vessel length category using  LENGTHCAT field
 
@@ -100,9 +89,15 @@ table2 <- left_join(eflalo_output, VE_lut)
 
 table1$VE_KW = as.numeric(table1$VE_KW)
 
+
+# make the data products directory if it doesn't exist
+dir.create("C:\\Users\\md09\\Documents\\git\\welsh_gov_fisheries_toolbox\\analysis_toolbox\\3_fisheries_data_products\\data-products")
+outPath = "C:\\Users\\md09\\Documents\\git\\welsh_gov_fisheries_toolbox\\analysis_toolbox\\3_fisheries_data_products\\data-products\\"
+
+
 # Table 1
 
-### Quarter, 0.05 Csquare, gear, metier
+### Quarter, 0.05 Csquare, gear
 
 table1.1 = 
   table1 %>%
@@ -126,17 +121,10 @@ table1.1 =
   #mutate(AverageGearWidth = NA %>% as.numeric()) %>%
   as.data.frame()
 
+write.csv(table1.1, paste0(outPath, "table1_1.csv"))
 
 
-
-
-
-
-
-write.xlsx(table1.1, "./data-products/table1.xlsx",sheetName = "1.1", row.names = F, append = T)
-
-
-### Year, 0.05 Csquare, gear, metier
+### Year, 0.05 Csquare, gear
 table1.2 = 
   table1 %>%
   group_by(VE_COU,Year,Csquare,LE_GEAR) %>%
@@ -159,14 +147,14 @@ table1.2 =
   mutate(AverageGearWidth = NA %>% as.numeric()) %>%
   as.data.frame()
 
-write.xlsx(table1.2, "./data-products/table1.xlsx",sheetName = "1.2", row.names = F, append = T)
+write.csv(table1.2, paste0(outPath, "table1_2.csv"))
 
 
-### Month, 0.05 Csquare, gear, metier
+### Month, 0.05 Csquare, gear
 
 table1.3 = 
   table1 %>%
-  group_by(VE_COU,Year,Csquare,LE_GEAR) %>%
+  group_by(VE_COU,Month,Csquare,LE_GEAR) %>%
   summarise(
     mean_si_sp       = mean(SI_SP),
     sum_intv         = sum(INTV, na.rm = TRUE),
@@ -186,18 +174,39 @@ table1.3 =
   mutate(AverageGearWidth = NA %>% as.numeric()) %>%
   as.data.frame()
 
-write.xlsx(table1.3, "./data-products/table1.xlsx",sheetName = "1.3", row.names = F, append = T)
+write.csv(table1.3, paste0(outPath, "table1_3.csv"))
 
 
-
-
-
-
-
-
-### Year, 0.01 Csquare, gear, metier, length category
+### Quarter, 0.01 Csquare, gear, length category
 
 table1.4 = 
+  table1 %>%
+  group_by(VE_COU,QUARTER,Csquare_01,LE_GEAR,LENGTHCAT) %>%
+  summarise(
+    mean_si_sp       = mean(SI_SP),
+    sum_intv         = sum(INTV, na.rm = TRUE),
+    mean_intv        = mean(INTV, na.rm=TRUE),
+    mean_ve_len      = mean(VE_LEN, na.rm = TRUE),
+    mean_ve_kf       = mean(VE_KW, na.rm = TRUE),
+    sum_kwHour       = sum(kwHour, na.rm=TRUE),
+    sum_le_kg_tot    = sum(LE_KG_TOT, na.rm = TRUE),
+    #sum_le_euro_tot  = sum(LE_EURO_TOT, na.rm = TRUE),
+    n_vessels        = n_distinct(VE_ID, na.rm = TRUE),
+    vessel_ids       = ifelse (
+      n_distinct(VE_ID) < 3,
+      paste(unique(VE_ID), collapse = ";"),
+      'not_required'
+    )
+  ) %>% relocate(n_vessels, vessel_ids, .before = Csquare_01) %>%
+  #mutate(AverageGearWidth = NA %>% as.numeric()) %>%
+  as.data.frame()
+
+write.csv(table1.4, paste0(outPath, "table1_4.csv"))
+
+
+### Year, 0.01 Csquare, gear, length category
+
+table1.5 = 
   table1 %>%
   group_by(VE_COU,Year,Csquare_01,LE_GEAR,LENGTHCAT) %>%
   summarise(
@@ -219,74 +228,14 @@ table1.4 =
   #mutate(AverageGearWidth = NA %>% as.numeric()) %>%
   as.data.frame()
 
-write.xlsx(table1.4, "./data-products/table1.xlsx",sheetName = "1.4", row.names = F, append = T)
+write.csv(table1.5, paste0(outPath, "table1_5.csv"))
 
 
-### Year, 0.01 Csquare, metier, length category
-
-table1.5 = 
-  table1 %>%
-  group_by(VE_COU,Year,Csquare_01,LE_MET,LENGTHCAT) %>%
-  summarise(
-    mean_si_sp       = mean(SI_SP),
-    sum_intv         = sum(INTV, na.rm = TRUE),
-    mean_intv        = mean(INTV, na.rm=TRUE),
-    mean_ve_len      = mean(VE_LEN, na.rm = TRUE),
-    mean_ve_kf       = mean(VE_KW, na.rm = TRUE),
-    sum_kwHour       = sum(kwHour, na.rm=TRUE),
-    sum_le_kg_tot    = sum(LE_KG_TOT, na.rm = TRUE),
-    #sum_le_euro_tot  = sum(LE_EURO_TOT, na.rm = TRUE),
-    n_vessels        = n_distinct(VE_ID, na.rm = TRUE),
-    vessel_ids       = ifelse (
-      n_distinct(VE_ID) < 3,
-      paste(unique(VE_ID), collapse = ";"),
-      'not_required'
-    )
-  ) %>% relocate(n_vessels, vessel_ids, .before = Csquare_01) %>%
-  #mutate(AverageGearWidth = NA %>% as.numeric()) %>%
-  as.data.frame()
-
-write.xlsx(table1.5, "./data-products/table1.xlsx",sheetName = "1.5", row.names = F, append = T)
-
-
-
-### Month, 0.05 Csquare, metier
-
-table1.7 = 
-  table1 %>%
-  group_by(VE_COU,Month,Csquare,LE_GEAR ) %>%
-  summarise(
-    mean_si_sp       = mean(SI_SP),
-    sum_intv         = sum(INTV, na.rm = TRUE),
-    mean_intv        = mean(INTV, na.rm=TRUE),
-    mean_ve_len      = mean(VE_LEN, na.rm = TRUE),
-    mean_ve_kf       = mean(VE_KW, na.rm = TRUE),
-    sum_kwHour       = sum(kwHour, na.rm=TRUE),
-    sum_le_kg_tot    = sum(LE_KG_TOT, na.rm = TRUE),
-    #sum_le_euro_tot  = sum(LE_EURO_TOT, na.rm = TRUE),
-    n_vessels        = n_distinct(VE_ID, na.rm = TRUE),
-    vessel_ids       = ifelse (
-      n_distinct(VE_ID) < 3,
-      paste(unique(VE_ID), collapse = ";"),
-      'not_required'
-    )
-  ) %>% relocate(n_vessels, vessel_ids, .before = Csquare) %>%
-  #mutate(AverageGearWidth = NA %>% as.numeric()) %>%
-  as.data.frame()
-
-write.xlsx(table1.7, "./data-products/table1.xlsx",sheetName = "1.7", row.names = F, append = T)
-
-
-
-
-
-
-
-### Quarter, 0.01 Csquare, metier
+### Month, 0.01 Csquare, gear, length category
 
 table1.6 = 
   table1 %>%
-  group_by(VE_COU,QUARTER,Csquare_01,LE_GEAR ) %>%
+  group_by(VE_COU,Month,Csquare_01,LE_GEAR,LENGTHCAT) %>%
   summarise(
     mean_si_sp       = mean(SI_SP),
     sum_intv         = sum(INTV, na.rm = TRUE),
@@ -306,32 +255,24 @@ table1.6 =
   #mutate(AverageGearWidth = NA %>% as.numeric()) %>%
   as.data.frame()
 
-write.xlsx(table1.6, "./data-products/table1.xlsx",sheetName = "1.6", row.names = F, append = T) ##save as csv instead 
-
-
-
- 
-
-
+write.csv(table1.6, paste0(outPath, "table1_6.csv"))
 
 
 
 ### DATA PRODUCTS VISUALIZATION ###############
 
-
-
-
 ## bounding box area limits definition
 
+# find the min and max limits of the table's data
 lats = c ( min(table1$SI_LATI), max(table1$SI_LATI) ) 
 lons = c ( min(table1$SI_LONG), max(table1$SI_LONG) ) 
 
 ## Welsh boundaries
-
+# expand the limits to give a better field of view
 lats = c ( 51, 53.5) 
 lons = c ( -6, -2 ) 
 
- 
+# create a bounding box 
 coord_bbox = data.frame(lons, lats)
 bbox_aoi =  coord_bbox %>% 
   st_as_sf(coords = c("lons","lats"), crs = 4326) %>%
@@ -342,79 +283,170 @@ bbox_aoi =  coord_bbox %>%
 
 ## create the grids at different resolutions 
 
-cell_resolution = 0.05 
-csquare_resolution = 0.05
+cell_resolution_005 = 0.05 
+csquare_resolution_005 = 0.05
 
 ##spatial grid 0_05 
 
-grid_0_05  = st_make_grid( bbox_aoi,  cellsize = cell_resolution, square = TRUE, offset = c(min(lons),min(lats))  ) #%>% as(Class = "Spatial")
+grid_0_05  = st_make_grid( bbox_aoi,  cellsize = cell_resolution_005, square = TRUE, offset = c(min(lons),min(lats)))#%>% as(Class="Spatial")
  
 grid_0_05_centroid  = st_sf (grid_0_05) %>%  st_centroid (.) %>% st_coordinates() 
-grid_0_05csquares =  CSquare ( grid_0_05_centroid[,"X"], grid_0_05_centroid[,"Y"],  csquare_resolution )
+grid_0_05csquares =  CSquare ( grid_0_05_centroid[,"X"], grid_0_05_centroid[,"Y"],  csquare_resolution_005 )
 
 csquares_0_05_split_centroids = st_bind_cols( grid_0_05, 
-                                         grid_res = cell_resolution, 
+                                         grid_res = cell_resolution_005, 
                                          csquare = grid_0_05csquares, 
                                          lon = round ( grid_0_05_centroid[,"X"],5 ), 
                                          lat = round ( grid_0_05_centroid[,"Y"],5)  )
 
 ##spatial grid 0_01
 
-cell_resolution = 0.01 
-csquare_resolution = 0.01
+cell_resolution_001 = 0.01 
+csquare_resolution_001 = 0.01
 
-grid_0_01  = st_make_grid( bbox_aoi,  cellsize = cell_resolution, square = TRUE, offset = c(min(lons),min(lats))  ) #%>% as(Class = "Spatial")
+grid_0_01  = st_make_grid( bbox_aoi,  cellsize = cell_resolution_001, square = TRUE, offset = c(min(lons),min(lats)))#%>% as(Class="Spatial")
 
 grid_0_01_centroid  = st_sf (grid_0_01) %>%  st_centroid (.) %>% st_coordinates() 
-grid_0_01csquares =  CSquare ( grid_0_01_centroid[,"X"], grid_0_01_centroid[,"Y"],  csquare_resolution )
+grid_0_01csquares =  CSquare ( grid_0_01_centroid[,"X"], grid_0_01_centroid[,"Y"],  csquare_resolution_001 )
 
 csquares_0_01_split_centroids = st_bind_cols( grid_0_01, 
-                                              grid_res = cell_resolution, 
+                                              grid_res = cell_resolution_001, 
                                               csquare = grid_0_01csquares, 
                                               lon = round ( grid_0_01_centroid[,"X"],5 ), 
                                               lat = round ( grid_0_01_centroid[,"Y"],5)  )
 
-
-
-
-
-
-
-
-
 ## Plot the tables 
 
-
-world <- ne_countries(scale = "large", returnclass = "sf")
-
-
-
-## Add the spatial grid geomtry to the fisheries data products tables: 
-
-table1.1_geom = csquares_0_05_split_centroids%>%inner_join(table1.1 , by = c("csquare" = "Csquare"))
-st_write(table1.1_geom, ) ##save as geojson 
-
-ggplot() + geom_sf( data = world   ) +
-  geom_sf (data = table1.1_geom  , aes(color = sum_intv,  fill = sum_intv) ) +
-  theme_bw() +
-  coord_sf(xlim = lons, ylim = lats, expand = FALSE) + 
-  facet_wrap( ~ LE_GEAR + QUARTER)
+# read in the world shapefile, or use the ne_countries function from the natural earth package if available
+world = read_sf("C:\\Users\\md09\\OneDrive - CEFAS\\data\\europe_coastline_shp", "Europe_coastline") %>% st_transform(., crs = 4326)
+#world <- ne_countries(scale = "large", returnclass = "sf")
 
 
+## Add the spatial grid geometry to the fisheries data products tables: 
+
+table1.1_geom = csquares_0_05_split_centroids %>% inner_join(table1.1, by = c("csquare" = "Csquare"))
+table1.2_geom = csquares_0_05_split_centroids %>% inner_join(table1.2, by = c("csquare" = "Csquare"))
+table1.3_geom = csquares_0_05_split_centroids %>% inner_join(table1.3, by = c("csquare" = "Csquare"))
+table1.4_geom = csquares_0_01_split_centroids %>% inner_join(table1.4, by = c("csquare" = "Csquare_01"))
+table1.5_geom = csquares_0_01_split_centroids %>% inner_join(table1.5, by = c("csquare" = "Csquare_01"))
+table1.6_geom = csquares_0_01_split_centroids %>% inner_join(table1.6, by = c("csquare" = "Csquare_01"))
+
+# create a folder to save the geojsons to
+dir.create(paste0(outPath, "geojson"))
+gjPath = paste0(outPath, "geojson\\")
 
 
+st_write(table1.1_geom, paste0(gjPath, "table1_1_geom.geojson"), layer = "table1_1_geom.geojson") ## save as geojson 
+st_write(table1.2_geom, paste0(gjPath, "table1_2_geom.geojson"), layer = "table1_2_geom.geojson")
+st_write(table1.3_geom, paste0(gjPath, "table1_3_geom.geojson"), layer = "table1_3_geom.geojson")
+st_write(table1.4_geom, paste0(gjPath, "table1_4_geom.geojson"), layer = "table1_4_geom.geojson")
+st_write(table1.5_geom, paste0(gjPath, "table1_5_geom.geojson"), layer = "table1_5_geom.geojson")
+st_write(table1.6_geom, paste0(gjPath, "table1_6_geom.geojson"), layer = "table1_6_geom.geojson")
 
-table1.6_geom = csquares_0_01_split_centroids%>%inner_join(table1.6 , by = c("csquare" = "Csquare_01"))
+# create a directory for plot outputs
+dir.create(paste0(outPath, "plots"))
+plotsPath = paste0(outPath, "plots\\")
+
+# create and save the plot images
+res1 = plot_function(data_plot = table1.1_geom, col_plot = 'sum_intv', col_facet1 = "LE_GEAR", col_facet2 = "QUARTER")
+ggsave(plot = res1, filename = paste0("table1_1_plot.png"), path = plotsPath, width = 16, height = 9, dpi = 300)
+
+res2 = plot_function(data_plot = table1.2_geom, col_plot = 'sum_intv', col_facet1 = "LE_GEAR", col_facet2 = "Year")
+ggsave(plot = res2, filename = paste0("table1_2_plot.png"), path = plotsPath, width = 16, height = 9, dpi = 300)
+
+res3 = plot_function(data_plot = table1.3_geom, col_plot = 'sum_intv', col_facet1 = "LE_GEAR", col_facet2 = "Month")
+ggsave(plot = res3, filename = paste0("table1_3_plot.png"), path = plotsPath, width = 16, height = 9, dpi = 300)
+
+res4 = plot_function(data_plot = table1.4_geom, col_plot = 'sum_intv', col_facet1 = "LE_GEAR", col_facet2 = "QUARTER")
+ggsave(plot = res4, filename = paste0("table1_4_plot.png"), path = plotsPath, width = 16, height = 9, dpi = 300)
+
+res5 = plot_function(data_plot = table1.5_geom, col_plot = 'sum_intv', col_facet1 = "LE_GEAR", col_facet2 = "Year")
+ggsave(plot = res5, filename = paste0("table1_5_plot.png"), path = plotsPath, width = 16, height = 9, dpi = 300)
+
+res6 = plot_function(data_plot = table1.6_geom, col_plot = 'sum_intv', col_facet1 = "LE_GEAR", col_facet2 = "Month")
+ggsave(plot = res6, filename = paste0("table1_6_plot.png"), path = plotsPath, width = 16, height = 9, dpi = 300)
 
 
-
-ggplot() + geom_sf( data = world   ) +
-  geom_sf (data = table1.6_geom  , aes(color = sum_intv,  fill = sum_intv) ) +
-  theme_bw() +
-  coord_sf(xlim = lons, ylim = lats, expand = FALSE)
-
-
-
+# plots = seq(1:6)
+# 
+# for (p in plots) {
+# 
+#   t = st_read(paste0("./data-products/geojson/table1_", p, ".geojson"))
+# 
+#   ggplot() + geom_sf( data = world   ) +
+#     geom_sf (data = t , aes(color = sum_intv,  fill = sum_intv) ) +
+#     theme_bw() +
+#     coord_sf(xlim = lons, ylim = lats, expand = FALSE) +
+#     facet_wrap( ~ LE_GEAR + colnames(t)[6])
+# 
+#   ggsave(filename = paste0("table1_", p, "_plot.png"), path = "./data-products/plots/")
+# 
+#   rm(t)
+# 
+#   print(paste0("Finished plotting table1_", p))
+# }
+# 
+# ################################################################################
+# 
+# years = seq(2009:2022)
+# 
+# for (y in years) {
+#   
+#   dir.create(paste0("./data-products/plots/", y))
+#   
+#   plotsQ = c(1,4)
+#   
+#   for (p in plotsQ) {
+#     
+#     t = st_read(paste0("./data-products/geojson/table1_", p, ".geojson"))
+#     
+#     
+#     tc = colnames(t)[6]
+#     res = plot_function(data_plot = table1.1, col_plot = 'sum_intv', col_facet1 = "LE_GEAR", col_facet2 = tc)
+#     
+#     
+#     ggsave(filename = paste0("table1_", p, "_plot_", y, ".png"), path = "./data-products/plots/")
+#     
+#     rm(t)
+#     
+#     print(paste0("Finished plotting table1_", p, "for year ", y))
+#   }
+#   
+#   
+#   plotsY = c(2,5)
+#   
+#   for (p in plots2Y) {
+#     
+#     t = st_read(paste0("./data-products/geojson/table1_", p, ".geojson"))
+#     
+#     plot_function(data_plot = table1.1, col_plot =  'sum_intv',col_facet1 = "LE_GEAR",col_facet2 =  "Year")
+#     
+#     ggsave(filename = paste0("table1_", p, "_plot_", y, ".png"), path = "./data-products/plots/")
+#     
+#     rm(t)
+#     
+#     print(paste0("Finished plotting table1_", p, "for year ", y))
+#   }
+#   
+#   plotsM = c(3,6)
+#   
+#   for (p in plotsM) {
+#     
+#     t = st_read(paste0("./data-products/geojson/table1_", p, ".geojson"))
+#     
+#     ggplot() + geom_sf( data = world   ) +
+#       geom_sf (data = t , aes(color = sum_intv,  fill = sum_intv) ) +
+#       theme_bw() +
+#       coord_sf(xlim = lons, ylim = lats, expand = FALSE) + 
+#       facet_wrap( ~ LE_GEAR + Month)
+#     
+#     ggsave(filename = paste0("table1_", p, "_plot_", y, ".png"), path = "./data-products/plots/")
+#     
+#     rm(t)
+#     
+#     print(paste0("Finished plotting table1_", p, "for year ", y))
+#   }
+# }
 
 ## 2 more examples of graphs 
 
