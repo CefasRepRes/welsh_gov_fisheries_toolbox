550248b5f67b0a35cdc5e9d3542a0fc57a05fb48
Roi Martinez (Cefas) <roi.martinez@cefas.gov.uk>
2023-06-21 13:48:01 +0100

    changes in code

--- analysis_toolbox/global.R
+++ analysis_toolbox/global.R
@@ -7,6 +7,7 @@ library(sf)         ## R Package for spatial analysis in R ( Simple Features )
 library(ggplot2)    ## R Package for plotitng and graphs
 
 library(vmstools)
+library( rnaturalearth )
 
 
 outPath = 'C:/Users/RM12/OneDrive - CEFAS/Roi/projects/Welsh_Government_Fishing/welsh_gov_fishing_analysis_capacity/data/data_output'
