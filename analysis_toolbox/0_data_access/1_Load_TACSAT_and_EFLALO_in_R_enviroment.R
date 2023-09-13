2fc714c0e0d738a8354b6caf7b2fa264284f9007
Mike Day <michael.day@cefas.gov.uk>
2023-09-13 09:10:09 +0100

    remove lines input by git from rebase

--- analysis_toolbox/0_data_access/1_Load_TACSAT_and_EFLALO_in_R_enviroment.R
+++ analysis_toolbox/0_data_access/1_Load_TACSAT_and_EFLALO_in_R_enviroment.R
@@ -191,13 +191,13 @@ if ( analysis_type == 'welsh_waters' ) {
   eflalo = eflalo %>% filter ( FT_REF  %in%  trips_in_welsh_waters) 
   tacsat = tacsat  %>% filter ( SI_FT  %in% trips_in_welsh_waters ) 
   
-<<<<<<< HEAD
+
   ## To visualize in a GIS software save the TACSAT  as point geometry 
   
   
-=======
+
   dir.create(".\\workflow_outputs\\spatial")
->>>>>>> 032832712aca887dd4fb88edce213b6c2885e459
+
   st_write( tacsat_geom_ww, dsn = ".\\workflow_outputs\\spatial\\tacsat_welsh_waters.geojson", layer = "tacsat_welsh_waters.geojson")
   
   
